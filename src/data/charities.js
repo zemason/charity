4c75bdce9901136739f6a34dd1a1048ac61f71db
zemason <zedromason@gmail.com>
2023-07-13 15:25:39 +0700

    Convert HTML to svelte component

---
+++ src/data/charities.js
@@ -0,0 +1,17 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/65535/50579862426_1b0d8e5c6f_o.jpg',
+            pledged: 0,
+            target: 100000,
+            date_end: +new Date('10 August 2023'),
+            profile_photo: 'https://live.staticflickr.com/65535/50579862426_1b0d8e5c6f_o.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+
+        }
+    ]
+}
